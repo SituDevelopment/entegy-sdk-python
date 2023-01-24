f82b6ae3b54dc17fd73f51f70f574f14cb854433
BlackIsBlack <blackout99mc@gmail.com>
2023-01-24 11:13:51 +1000

    Enumerator for document icons

--- src/entegywrapper/schemas/content.py
+++ src/entegywrapper/schemas/content.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import Literal, TypeAlias, TypedDict
 
 from . import pageSettings, stringKeys
@@ -29,17 +30,83 @@ TemplateType: TypeAlias = Literal[
 ]
 
 
+class Icon(int, Enum):
+    DOCUMENT: 0
+    FACEBOOK: 1
+    LINKEDIN: 2
+    TWITTER: 3
+    MAPPIN: 4
+    GLOBE: 5
+    ENEVELOPE: 6
+    PHONE: 7
+    SPEAKER: 8
+    FILMREEL: 9
+    DESKTOP: 10
+    CHAT: 11
+    MAP: 12
+    ARTICLE: 13
+    GALLERY: 14
+    OUTBOX: 15
+    LINKEDIN: 16
+    PIN: 17
+    BARGRAPH: 18
+    PAPERCLIP: 19
+    ACCESSPOINT: 20
+    CALENDAR: 21
+    CHECKMARK: 22
+    PAPERPLANE: 23
+    STAR: 24
+    WARNINGTRIANGLE: 25
+    SEARCH: 26
+    FOLDER: 27
+    INBOX: 28
+    EDIT: 31
+    PEOPLE: 32
+    HOME: 37
+    HOMESEARCH: 38
+    DINER: 39
+    OPENSIGN: 40
+    INFOCIRCLE: 41
+    HELPCIRCLE: 42
+    FLAG: 43
+    WINEGLASS: 44
+    COCKTAIL: 45
+    COFFEE: 46
+    NEWS: 47
+    CAR: 48
+    OFFICEBUILDING: 49
+    INFOSQUARE: 52
+    GLOBESEARCH: 53
+    ALARMCLOCK: 54
+    GUITAR: 55
+    ROADSIGNS: 56
+    WIFITABLET: 57
+    OPENBOOK: 58
+    XBOX: 62
+    GEAR: 64
+    LOGOUT: 65
+    INSTAGRAM: 68
+    GOOGLEPLUS: 69
+    HASHTAG: 70
+    BACK: 71
+    FORWARD: 72
+    REFRESH: 73
+    DOCUMENTSTAMP: 75
+    ALARMBELL: 77
+    ALARMBELLCOG: 78
+
+
 class Document(TypedDict):
     name: str
     externalReference: str
-    icon: int
+    icon: Icon
     fileUrl: str
 
 
 class ExternalContent(TypedDict):
     name: str
     externalReference: str
-    icon: int
+    icon: Icon
     fileUrl: str
     type: str
 
